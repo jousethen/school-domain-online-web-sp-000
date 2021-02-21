fb10c33d2ce7d32271a060e9d17860a95162d21e
Jouse then <jthen93@gmail.com>
2021-02-21 23:29:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,9 @@ class School
   end
   
   def add_student (sname, grade)
-    if self.roster.has_key?(grade) == false
+
       self.roster[grade] = [] 
-    end
+    
     self.roster[grade] << sname
   end
 end
